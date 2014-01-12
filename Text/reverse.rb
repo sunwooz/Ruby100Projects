14c26508953792fb29eaa09293b2d55e7c4d7eee
Daniel Yang <yangsunwoo@gmail.com>
2014-01-12 08:10:30 -0500

    added reverse string

---
+++ Text/reverse.rb
@@ -0,0 +1,7 @@
+# Reverse a String – Enter a string and the program will reverse it and print it out.
+
+puts 'Enter a string to be reversed.'
+
+string_input = gets.chomp
+
+puts string_input.reverse
